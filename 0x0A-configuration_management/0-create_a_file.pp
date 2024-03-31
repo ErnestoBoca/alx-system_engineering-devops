b4510f6a21f77a59ec339c96aa51b56f83fd1070
Ernesto Jordão Boca <99892374+ErnestoBoca@users.noreply.github.com>
2024-03-31 16:11:10 +0200

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,8 +0,0 @@
-# creates a new file with puppet
-file { '/tmp/school':
-  ensure  => present,
-  content => 'I love Puppet',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-}

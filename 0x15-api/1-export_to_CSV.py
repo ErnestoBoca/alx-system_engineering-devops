7eece9c838346730c58e902beb83f275fe8307c6
Ernesto Boca <boca.ernesto13@gmail.com>
2024-09-25 13:39:00 -0700

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""This script uses a REST API, and for a given employee ID,
+returns information about his/her TODO list progress."""
+import requests
+import sys
+
+
+def get_todo_list():
+    """Prints the todo list for a given eployee"""
+    user_id = sys.argv[1]
+
+    tasks_params = {'userId': user_id}
+    tasks = requests.get("https://jsonplaceholder.typicode.com/todos",
+                         params=tasks_params).json()
+
+    user_params = {'id': user_id}
+    user = requests.get("https://jsonplaceholder.typicode.com/users",
+                        params=user_params).json()
+
+    print("Employee {} is done with tasks({}/{}):".format(
+          user[0].get('name'), number_of_done(tasks), len(tasks)))
+    for task in tasks:
+        if task.get('completed'):
+            print("\t {}".format(task.get('title')))
+
+
+def to_csv():
+    """exports data in the CSV format"""
+    user_id = sys.argv[1]
+
+    tasks_params = {'userId': user_id}
+    tasks = requests.get("https://jsonplaceholder.typicode.com/todos",
+                         params=tasks_params).json()
+
+    user_params = {'id': user_id}
+    user = requests.get("https://jsonplaceholder.typicode.com/users",
+                        params=user_params).json()[0]
+
+    file_name = "{}.csv".format(user_id)
+    with open(file_name, 'w', encoding='utf-8') as csv_file:
+        for task in tasks:
+            csv_file.write('"{}","{}","{}","{}"\n'.format(
+                            user_id, user.get('username'),
+                            task.get('completed'), task.get('title')))
+
+
+def number_of_done(tasks):
+    """Return the number of done tasks"""
+    done = 0
+    for task in tasks:
+        if task.get('completed'):
+            done += 1
+    return done
+
+
+if __name__ == "__main__":
+    to_csv()
